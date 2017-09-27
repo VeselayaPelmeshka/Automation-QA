254213c9ee3acf9b4f1e7c9463d03e01897bc61d
VeselayaPelmeshka <veselayapelmeshka@gmail.com>
2017-09-27 17:30:13 +0300

    ДЗ по второму уроку

---
+++ src/main/java/lesson2/Solution2.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Solution2 {
+
+
+    public static void main(String[] args) {
+        double n = 876876.37;
+        int resusl = (int)Math.round(n);
+        System.out.println(resusl);
+    }
+
+}
